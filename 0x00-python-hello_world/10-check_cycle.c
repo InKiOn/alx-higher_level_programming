ca5b813ab93b8deba0a471b256629ed0da62b107
InKiOn <122981923+InKiOn@users.noreply.github.com>
2023-05-08 21:08:16 +0000

    Task files

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - Checks if a singly-linked list has a cycle
+ * @list: singly-linked list
+ *
+ * Return: 0 if no cycle, else 1
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *h;
+	listint_t *t;
+
+	if (list == NULL || list->next == NULL)
+	{
+		return (0);
+	}
+	t = list->next;
+	h = list->next->next;
+
+	while (t && h && h->next)
+	{
+		if (t == h)
+		{
+			return (1);
+		}
+		t = t->next;
+		h = h->next->next;
+	}
+	return (0);
+}
---
+++ 0x00-python-hello_world/README.md
@@ -0,0 +1,5 @@
+# 0x00-python-hello_world
+* 8/5/2023
+
+## General info
+Repository for python files for 0x00. Python - Hello, World
